ac59044d44446fd0b819332c2d33b93aab3000b4
WOLF_JK <279408285@qq.com>
2020-04-19 20:27:55 +0800

    ew

--- kern/monitor.c
+++ kern/monitor.c
@@ -58,31 +58,7 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	/*
-	// Your code here.
-    uint32_t *ebp = (uint32_t *)read_ebp();
-    uint32_t *eip = (uint32_t *)ebp[1];
-    uint32_t args[5], i;
-	struct Eipdebuginfo dbg_info;
-    for (i = 0; i < 5; i++)
-        args[i] = ebp[i + 2];
-    
-    cprintf("Stack_backtrace:\n");
-    while (ebp != NULL)
-    {
-        cprintf(" ebp:0x%08x eip:0x%08x args:0x%08x 0x%08x 0x%08x 0x%08x 0x%08x\n",
-            ebp, eip, args[0], args[1], args[2], args[3], args[4]);
-		debuginfo_eip((uintptr_t)eip, &dbg_info);
-		// 获取信息
-
-		cprintf("\t%s:%d %.*s+%d\n", dbg_info.eip_file, dbg_info.eip_line, dbg_info.eip_fn_namelen, dbg_info.eip_fn_name, ebp[1] - dbg_info.eip_fn_addr);
-        ebp = (uint32_t *)ebp[0];
-        eip = (uint32_t *)ebp[1];
-        for (i = 0; i < 5; i++)
-            args[i] = ebp[i + 2];
-    }
-    return 0;*/
-	// Your code here.
+	
 	uint32_t *ebp = (uint32_t *)read_ebp();
 	struct Eipdebuginfo eipdebuginfo;
 	while (ebp != 0) {
